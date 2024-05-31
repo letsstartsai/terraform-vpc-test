2ca409c0b4722cb3c81fe63552529fa7253b76f6
lets start sai <info@sai.com>
2024-05-31 12:51:07 +0530

    about to commit

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "project_name" {
+    default = "expense"
+    
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+    }
+  
+}
